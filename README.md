94ce4e947c421528fd571d6eb5c47bc958274a79
Peter Schrader <peter.schrader2009@gmail.com>
2020-12-04 14:06:30 -0500

    initial work

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Capstone-2
+**Predicting NHL Game Outcomes - The Data**
+* 7 seasons from 2011 – 2018
+* 11,434 games
+* Broken down by team and game statistics
+* Average of 5.55 goals per game
+* Average spread 2.05 goals
+* Which statistics or features best predict game outcomes?
+
+**So What?**
+* Sports gambling is a 150 billion dollar industry. (ref 1)
+* Profits can be made above around 61.5% accuracy prediction.
+* Other Models (ref 2):
+* Top test accuracy: 60.3% with “Logistic 3”
+* Average accuracy: 59.3%
+* Mentions of models performing around 62
+
+![alt text](img/other_models.png "This is a title")
+![alt text](img/model_diagram.png "This is a title")
+
+* Images from corsicahockey.com
+
+**Models - All Seasons**
+* Trained Logistic Regression, Random Forest, and Gradient Boost models on all seasons in the data set.
+
+![alt text](img/Cross_val_all_season_train.png "This is a title")
+
+* Gradient Boost had highest average cross-val score, Random Forest had highest overall score for one season.
+
+**Models – One Season**
+* Trained Random Forest and Gradient Boost on 1 season (2016-2017).
+* Feature importances:
+
+![alt text](img/feat_importances.png "This is a title")
+
+* No features really jump out.
+* Next, trained the models on one season and predicted other seasons.
+
+![alt text](img/one_season_training.png "This is a title")
+
+* Now Random Forest performed best overall and on average but accuracy didn't improve a whole lot for either.
+
+**Conclusions**
+* Don’t bet money based on this model.
+* Hockey is difficult to predict.
+* Add in player stats to as features.
+
+**Lessons Learned and Further Research**
+* EDA took longer than expected - almost 3 full days.
+* Only used Grid Search to optimize Random Forest and Gradient Boost models.  Try Random Search and also XGBoost in the future.
+* Use a neural network.  Issues with deep learning took longer than expected.
+* Optimize a pipeline for cleaning the data.
+* This project needs significant re-work.
+* Clean up notebooks, utilize src, more time on modeling to achieve 60%.
+
+
+**References**
+<ul>1. "How Much Money Do Americans Bet On Sports?", Legal Sports Betting, Nov 8, 2020,  https://www.legalsportsbetting.com/how-much-money-do-americans-bet-on-sports. 
+<br>   2. "Corsica Predictions Explained", Corsica Hockey, 2020, https://www.corsicahockey.com/corsica-predictions-explained.
+<\ul>
---
+++ src/helper.py
@@ -0,0 +1,53 @@
+def seasons(df, season):
+    
+    filtered = season_team[season_team.season == season]
+
+    # now calc cumulative stats
+    filtered['gpg'] = (filtered.groupby('team_id')['goals'].cumsum() - filtered['goals']) /           filtered.groupby('team_id').cumcount()
+    filtered['win_per'] = (filtered.groupby('team_id')['won'].cumsum() - filtered['won']) / filtered.groupby('team_id').cumcount()
+    filtered['shots_pg'] = (filtered.groupby('team_id')['shots'].cumsum() - filtered['shots']) / filtered.groupby('team_id').cumcount()
+    filtered['hits_pg'] = (filtered.groupby('team_id')['hits'].cumsum() - filtered['hits']) / filtered.groupby('team_id').cumcount()
+    filtered['pim_pg'] = (filtered.groupby('team_id')['pim'].cumsum() - filtered['pim']) / filtered.groupby('team_id').cumcount()
+    filtered['hits_pg'] = (filtered.groupby('team_id')['hits'].cumsum() - filtered['hits']) / filtered.groupby('team_id').cumcount()
+    filtered['fo_per'] = (filtered.groupby('team_id')['faceOffWinPercentage'].cumsum() - filtered['faceOffWinPercentage']) /        filtered.groupby('team_id').cumcount()
+    filtered['giveaways_pg'] = (filtered.groupby('team_id')['giveaways'].cumsum() - filtered['giveaways']) / filtered.groupby('team_id').cumcount()
+    filtered['takeaways_pg'] = (filtered.groupby('team_id')['takeaways'].cumsum() - filtered['takeaways']) / filtered.groupby('team_id').cumcount()
+    filtered['ppg_per'] = ((filtered.groupby('team_id')['powerPlayGoals'].cumsum() - filtered['powerPlayGoals'])) / ((filtered.groupby('team_id')['powerPlayOpportunities'].cumsum() - filtered['powerPlayOpportunities']))
+
+    return filtered
+
+def filter_home_away(df_filtered, df):
+    
+    # filter home team stats
+    filtered_h = df_filtered[df_filtered.HoA == 'home']
+    filtered_h = filtered_h.filter(items=['game_id', 'season', 'streak', 'time_travel', 'gpg', 'win_per', 'ppg_per', 'shots_pg', 'hits_pg', 'pim_pg', 'fo_per', 'giveaways_pg', 'takeaways_pg'])
+    filtered_h = filtered_h.rename(columns={'streak': 'home_streak', 'time_travel': 'home_time_travel', 'gpg': 'home_gpg', "win_per": "home_win_per", "ppg_per": "home_ppg_per", 'shots_pg': 'home_shots_pg', 'hits_pg': 'home_hits_pg', 'pim_pg': 'home_pim_pg', 'fo_per': 'home_fo_per', 'giveaways_pg': 'home_giveaways_pg',
+       'takeaways_pg': 'home_takeaways_pg'})
+    # merge home team stats
+    df = df.merge(filtered_h, on='game_id', how='left')
+
+    # filter away team stats
+    filtered_a = df_filtered[df_filtered.HoA == 'away']
+    filtered_a = filtered_a.filter(items=['game_id', 'season', 'streak', 'time_travel', 'gpg', 'win_per', 'ppg_per', 'shots_pg', 'hits_pg', 'pim_pg', 'fo_per', 'giveaways_pg', 'takeaways_pg'])
+    filtered_a = filtered_a.rename(columns={'streak': 'away_streak', 'time_travel': 'away_time_travel', 'gpg': 'away_gpg', "win_per": "away_win_per", "ppg_per": "away_ppg_per", 'shots_pg': 'away_shots_pg', 'hits_pg': 'away_hits_pg', 'pim_pg': 'away_pim_pg', 'fo_per': 'away_fo_per', 'giveaways_pg': 'away_giveaways_pg',
+       'takeaways_pg': 'away_takeaways_pg'})
+
+    # merge away team stats
+    df = df.merge(filtered_a, on='game_id', how='left')
+    
+    return df
+
+def replace_bools_fillna(df):
+    
+    df = df.drop(columns=['season_x', 'season_y', 'game_id', 'type', 'date_time', 'date_time_GMT'])
+    df = df.replace({'home_streak': False}, 0)
+    df = df.replace({'home_streak': True}, 1)
+    df = df.replace({'away_streak': False}, 0)
+    df = df.replace({'away_streak': True}, 1)
+    df = df.replace({'outcome': 'loss'}, 0)
+    df = df.replace({'outcome': 'win'}, 1)
+    df = df.fillna(0)
+    
+    return df
+
+### replace_bools_fillna(filter_home_away(seasons(season_20122013_test, season), season_20122013_test))
